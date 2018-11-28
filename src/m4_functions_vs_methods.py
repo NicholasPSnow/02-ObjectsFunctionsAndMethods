d94670d72ee2d89cefbd8bd23781d57ae3e92114
NicholasPSnow <44656056+NicholasPSnow@users.noreply.github.com>
2018-11-28 09:26:01 -0500

    M1,M2,M3

--- src/m4_functions_vs_methods.py
+++ src/m4_functions_vs_methods.py
@@ -4,11 +4,11 @@ Demonstrates using (calling) FUNCTIONS and using (calling) METHODS:
   -- how they differ.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Nicholas Snow.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #   With your instructor, READ the file   methods_vs_functions.txt
 #   in this project, ASKING QUESTIONS as needed to understand its contents.
 #   After you have done so, mark this _TODO_ as DONE
@@ -98,7 +98,7 @@ def draw_many_squares(my_turtle, number_of_squares, size, twist):
         my_turtle.left(twist)
 
 ###############################################################################
-# TODO: 3.
+# Done: 3.
 #   There are four FUNCTIONS defined ABOVE this:
 #     main
 #     run_example
@@ -136,10 +136,15 @@ def try_methods():
       -- backward  100 units
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function, per its doc-string above.
+    # Done: 4. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     ###########################################################################
-
+    turtwig=rg.SimpleTurtle()
+    turtwig.pen=rg.Pen('brown',5)
+    turtwig.forward(150)
+    turtwig.left(90)
+    turtwig.forward(50)
+    turtwig.backward(100)
 
 ###############################################################################
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
@@ -152,7 +157,7 @@ def try_functions():
      -- One jumps to (-50, 50), then moves (while drawing) to (100, 100)
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function, per its doc-string above.
+    # Done: 5. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #    NOTE: This function requires
@@ -160,7 +165,9 @@ def try_functions():
     #    If you think it needs more, ** ASK FOR HELP. **
     #    HINT: see   jump_and_move_turtle   above.
     ###########################################################################
-
+jump_and_move_turtle(200, 100, 300, 30)
+jump_and_move_turtle(100,200, 0, 0)
+jump_and_move_turtle(-50, 50, 100, 100)
 
 ###############################################################################
 # IMPORTANT: Read the NOTE below before you try to implement the next function!
@@ -198,13 +205,28 @@ def try_methods_and_functions():
       8. Draw a SQUARE whose sides are each of length 50.
     """
     ###########################################################################
-    # TODO: 6. Implement and test this function, per its doc-string above.
+    # Done: 6. Implement and test this function, per its doc-string above.
     #   The testing code (in main) is already written for you.
     #
     #   NOTE: This function should ** CALL ** the
     #     draw_many_squares
     #   function defined above.  If you don't see why, ** ASK FOR HELP. **
     ###########################################################################
+    bulbasaur=rg.SimpleTurtle()
+    bulbasaur.pen=rg.Pen('blue',5)
+    bulbasaur.backward(100)
+    bulbasaur.speed=1
+    draw_many_squares(bulbasaur,2,100,30)
+    bulbasaur.speed = 5
+    bulbasaur.pen.color = 'red'
+    draw_many_squares(bulbasaur,10, 50, 15)
+    bulbasaur.speed = 100
+    bulbasaur.pen.thickness = 35
+    draw_many_squares(bulbasaur,8, 300, 60)
+    bulbasaur.pen = rg.Pen('black', 3)
+    bulbasaur.backward(200)
+    bulbasaur.draw_circle(30)
+    bulbasaur.draw_square(50)
 
 
 # -----------------------------------------------------------------------------
